528776460756d33aa91b3d97e57868ad53c9ba0f
Mainafrancis <francis.maina@student.moringaschool.com>
2022-03-07 11:28:25 +0300

    progress

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ setInterval(setClock, 1000)
 function setClock(){
     const currentDate = new Date()
     const secondsRatio = currentDate.getSeconds() / 60
-    const minutesRatio = currentDate.getMinutes() / 60
-    const hoursRatio = currentDate.getHours() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
 }
